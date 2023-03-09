d0a378d4db33904fca37cd001d90fd13bdf6417a
Ruben Perez <ruben.prezjuarez@mail.mcgill.ca>
2023-03-09 13:24:55 -0500

    actualizacion de la fecha de la deadline

--- app.js
+++ app.js
@@ -25,7 +25,6 @@ const daysOfTheWeek = [
     "Saturday",
 ];
 
-
 // giveaway es el contenedor del texto que dice cuando sera llevado a cabo
 const giveaway = document.querySelector(".giveaway");
 //deadline s el gran contenedor, adentro tiene a los 4 cuadritos de cuenta regresiva
@@ -33,12 +32,9 @@ const deadline = document.querySelector(".deadline");
 // items equivale a todos los cuadritos donde aparece el tiempo que falta
 const items = document.querySelectorAll(".deadline-format h4");
 
-
-
-
 // to MANUALLY set up a specific date (hardcoded)
 //                 year, month(0-11), day, hour(0-24), minutes, seconds
-let futureDate = new Date(2023, 0, 31, 17, 30, 0); 
+let futureDate = new Date(2024, 11, 31, 17, 30, 0);
 
 /*  ****************************************************************
     Otra forma para poner una fecha de forma AUTOMATICA 
@@ -64,27 +60,20 @@ const date = futureDate.getDate();
 let weekday = futureDate.getDay();
 weekday = daysOfTheWeek[weekday];
 
-
-
 giveaway.textContent = `giveaway ends on ${weekday}, ${month} ${date} ${year} at ${hours}:${minutes}pm.`;
-
-
 
 // future time in milliseconds ms
 // .getTime()  nos da la fecha establecida como deadline arriba en milisegundos no en formato de fecha
 const futureTime = futureDate.getTime();
 // console.log(futureTime);
 
-
-
 // funcion para calcular el tiempo que queda. Vamos a restarle al tiempo futuro en milisegundos
 // el tiempo que hace ahorita en milisegundas
-function getRemainingTime(){
+function getRemainingTime() {
     const today = new Date().getTime(); // la fecha de hoy en milisegundos
     // console.log(today);
     const tiempo = futureTime - today;
     // console.log(tiempo);
-
 
     /**1 s = 1000ms
      * 1 min = 60 s
@@ -106,17 +95,15 @@ function getRemainingTime(){
     // console.log(hours);
 
     let minutes = Math.floor((tiempo % oneHour) / oneMinute);
-    // console.log(minutes);    
-    
+    // console.log(minutes);
+
     let seconds = Math.floor((tiempo % oneMinute) / oneSecond);
     // console.log(seconds);
 
-
-
     // para darle formato a los números cuando te queden 2 horas, por ejemplo, que muestre 02
-    function format(numero){
+    function format(numero) {
         if (numero < 10) {
-            return numero = `0${numero}`;
+            return (numero = `0${numero}`);
         }
         return numero;
     }
@@ -124,31 +111,21 @@ function getRemainingTime(){
     // aquí creamos un array con los resultados y sustituimos el relleno de items (arriba top)
     const values = [days, hours, minutes, seconds];
 
-    items.forEach(function(element, index) {
+    items.forEach(function (element, index) {
         element.innerHTML = format(values[index]);
     });
 
-    // si la cuenta regresiva llega a 0, para que no marque 0-02 como error, etc. 
+    // si la cuenta regresiva llega a 0, para que no marque 0-02 como error, etc.
     // Que el contador se detenga, se desaparezca y en su lugar aparezca un texto que diga
     // que ya terminô
     if (tiempo < 0) {
         clearInterval(countdown);
         deadline.innerHTML = `<h4 class="expired">sorry, this giveaway has expired<h4>`;
-    };
-
-
-};
+    }
+}
 
 // countdown
 let countdown = setInterval(getRemainingTime, 1000);
 
-
-
-
 //invocando la funcion. Importante que estê al final
-getRemainingTime(); 
-
-
-
-
-
+getRemainingTime();
